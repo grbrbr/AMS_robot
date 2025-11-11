314a34929b70e93a225bba4b9daf2f2ce1b8cf03
Gabrijela <grbrbr6@gmail.com>
2025-11-11 09:14:00 +0100

    vaja2

--- control_line.py
+++ control_line.py
@@ -6,16 +6,63 @@ from geometry_msgs.msg import Twist
 from amsagv_msgs.msg import LineStamped, TagStamped
 from math import pi, sin, cos, isnan
 from world import MTAG
+import math
+
+#math.isnan
 
 
 
 tag = None
 
+edgeSelection = "R"
+#edgeSelection = "L"
+
+
+P_val = 3.2
+
+v_default = 0.15
+
 # Handle line sensor
 def handleLine(msg):
-  print(msg.line.left, msg.line.right)
+  #print(msg.line.left, msg.line.right)
   
+  #TODO Implement line following control algorithm here ...
+
   v, w = 0.0, 0.0
+  valid = True
+  error = 0
+
+  if edgeSelection == "R":
+    reference = -0.48
+  elif edgeSelection == "L":
+    reference = 0.48
+
+  
+  if (msg.line.left == 1.0) and (msg.line.right == -1.0):
+    valid = False
+  
+  if edgeSelection == "R":
+      lineVal = msg.line.right
+  elif edgeSelection == "L":
+      lineVal = msg.line.left
+
+  if math.isnan(lineVal):
+    valid = False
+  
+
+  if valid:
+    
+    error = -(reference - lineVal)
+
+    #if abs(error) < 0.1:
+      #error=0
+
+    w = error * P_val
+    v = v_default
+
+  
+  
+  #print(f"lineval:{lineVal}, error:{error}, w:{w}")
 
   # Velocity commands message
   msgCmdVel = Twist()
@@ -28,8 +75,23 @@ def handleLine(msg):
 
 def handleTag(msg):
   global tag
+  global edgeSelection
   tag = MTAG.get(msg.tag.id, None)
+  
+  if tag == 20:
+    edgeSelection ="L" 
+  elif tag == 18:
+    edgeSelection ="R" 
+  elif tag == 17:
+    edgeSelection ="L" 
+  elif tag == 16:
+    edgeSelection ="R"
+
+
+
   print('New tag: {} -> {}'.format(msg.tag.id, tag))
+
+
 
 
 
